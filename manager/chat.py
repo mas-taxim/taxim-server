ae79f0aa643b598fc7479ffc608bd209d1c52e2b
joonas-yoon <joonas-yoon@gmail.com>
2023-06-18 13:15:59 +0900

    add sorry messages for empty

--- manager/chat.py
+++ manager/chat.py
@@ -1,3 +1,15 @@
+from random import choice
+
+SORRY_MESSAGES = [
+    "죄송합니다. 그 질문에 대한 답을 모르겠습니다.",
+    "저는 아직 개발 중이라 모든 질문에 답할 수 없습니다.",
+    "그 질문은 저에게 너무 어렵습니다.",
+    "뭐라고 말해야 할지 잘 모르겠습니다.",
+    "대답할 수 없는 유형의 질문입니다.",
+    "죄송합니다. 그것에 대한 대답은 아직 제공할 수 없습니다.",
+    "아직은 답변이 불가한 유형입니다. 더 나은 서비스로 찾아뵙겠습니다."
+]
+
 
 def hello(text: str) -> str:
     return "Hello Taxim!"
@@ -9,10 +21,10 @@ def response(in_text: str):
         focus = "V5"
     elif "도난" in in_text and "의심" in in_text:
         out_text = "현재 도난 의심 차량은 없습니다."
-        focus = ""
+        focus = None
     else:
-        out_text = ""
-        focus = ""
+        out_text = choice(SORRY_MESSAGES)
+        focus = None
     return {
         "text": out_text,
         "focus": focus
